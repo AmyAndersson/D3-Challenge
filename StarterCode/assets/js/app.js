5b0a67bd1eb098bb80ed733d6061e52efc93471a
AmyAndersson <66049569+AmyAndersson@users.noreply.github.com>
2020-09-26 08:37:46 +0800

    initial scatter plot

---
+++ StarterCode/assets/js/app.js
@@ -0,0 +1,138 @@
+// Step 1: Set up our chart
+//= ================================
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 60,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Step 2: Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+// =================================
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+  var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  
+
+
+// Step 3:
+// Import data from the data.csv file
+// =================================
+d3.csv("assets/data/data.csv").then(function(HealthData) {
+
+  // step 4: scales
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(HealthData, d => +d.poverty))
+    .range([0, width])
+
+
+  const yScale = d3.scaleLinear()
+    .domain([0,d3.max(HealthData, d => +d.healthcare)])
+    .range([height, 0]);
+
+  
+  // Step 5: create axis
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+ 
+// Step 6: append axis to chart 
+chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(xAxis);
+chartGroup.append("g").call(yAxis);
+
+//Step 7: Scatter plot 
+chartGroup.selectAll("circle")
+.data(HealthData)
+.enter()
+.append("circle")
+.attr("cx", d=>xScale(+d.poverty))
+.attr("cy", d=>yScale(+d.healthcare))
+.attr("r", "10")
+.attr("stroke-width", "1")
+.classed("stateCircle", true)
+.attr("opacity", 0.75);
+
+
+//Step 8: Add data to each point 
+chartGroup.append("g")
+.selectAll('text')
+.data(HealthData)
+.enter()
+.append("text")
+.text(d=>d.abbr)
+.attr("x",d=>xScale(+d.poverty))
+.attr("y",d=>yScale(+d.healthcare))
+.classed(".stateText", true)
+.attr("font-family", "sans-serif")
+.attr("text-anchor", "middle")
+.attr("fill", "white")
+.attr("font-size", "10px")
+.style("font-weight", "bold")
+.attr("alignment-baseline", "central");
+
+//Step 9 Add axis details 
+chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 13})`)
+      .text("Poverty (%)");
+
+      chartGroup.append("text")
+      .attr("y", 0 - ((margin.left / 2) + 2))
+      .attr("x", 0 - (height / 2))
+      .attr("transform", "rotate(-90)")
+      .text("Healthcare (%)");
+
+
+      // Exrtra trial
+
+
+  // Step 1: Append a div to the body to create tooltips, assign it a class
+  // =======================================================
+  var toolTip = d3.select("body").append("div")
+    .attr("class", "tooltip");
+
+  // Step 2: Add an onmouseover event to display a tooltip
+  // ========================================================
+  chartGroup.on("mouseover", function() {
+    toolTip.style("display", "block");
+    toolTip.html(`State: <strong>${HealthData, d => d.state[i]}</strong>`)
+      .style("left", d3.event.pageX + "px")
+      .style("top", d3.event.pageY + "px");
+  });
+    // Step 3: Add an onmouseout event to make the tooltip invisible
+    .on("mouseout", function() {
+      toolTip.style("display", "none");
+    });
+
+
+
+
+
+
+var marker = svg.append('circle')
+  .attr('r', 7)
+  .style('display', 'none')
+  .style('fill', '#FFFFFF')
+  .style('pointer-events', 'none')
+  .style('stroke', '#FB5050')
+  .style('stroke-width', '3px');
+
+
+
+
+}).catch(function(error) {
+console.log(error);
+}) 
